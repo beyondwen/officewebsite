c8dd5523b86ce117a954ebf9b43b8380ea60f2e6
beyond <921790476@qq.com>
2020-03-30 11:17:26 +0800

    添加

--- src/main/java/com/amchis/officewebsite/domain/Articlecontent.java
+++ src/main/java/com/amchis/officewebsite/domain/Articlecontent.java
@@ -24,7 +24,7 @@ public class Articlecontent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Integer id;
 
     /**
      * 文章标题
